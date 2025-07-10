eae7f52c10433282a6dda037e66ea3f9f7aeb7e2
HrafovMaksym <mgrafov06@gmail.com>
2025-07-10 22:36:40 +0200

    set up registration

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,11 +1,47 @@
+import bcrypt from "bcrypt";
+
+import { Prisma } from "@prisma/client";
 import { Request, Response } from "express";
+import { prisma } from "../config/prisma.client";
+import { jwtService } from "../services/jwt.service";
 
 export const authController = {
-  async register(req: Request, res: Response) {
+  async registration(req: Request, res: Response) {
     try {
+      const { email, password, name } = await req.body;
+      console.log(email, password, name);
+      if (!email || !password || !name)
+        return res.status(400).json({ message: "Data is missing" });
+
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      const user = await prisma.user.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+        },
+      });
+      console.log("user", user);
+      const token = await jwtService.generateToken(user);
+
+      res.cookie("auth_token", token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      });
+      return res.status(201).json({ message: "User created successfully" });
     } catch (error) {
-      console.log("Error while registering:", error);
-      res.status(500).json({ message: "Internal server error" });
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === "P2002"
+      ) {
+        res.status(400).json({ message: "User already exists" });
+      } else {
+        res.status(500).json({ message: "Internal server error" });
+      }
     }
   },
 };
